cb8645a632ee6e33a129c6ed48458ff40537020b
Lanjkn <86944753+Lanjkn@users.noreply.github.com>
2022-05-07 23:41:59 -0300

    Now in english! and some other stuff
    the CRUD is now in english!
    it i'll maintain it that way.
    pretty much finished the whole project, there's nothing more to it.
    If you want to use it, feel free, but i'm sure you can do something better in a couple minutes!

--- agenda.py
+++ agenda.py
@@ -1,110 +1,147 @@
-AGENDA = {}
 
 
-def mostraContatos():
-    if(AGENDA):
-        for contato in AGENDA:
-            buscaContatos(contato)
+CONTACTLIST = {}
+
+
+def showContacts():
+    if(CONTACTLIST):
+        for contact in CONTACTLIST:
+            searchContacts(contact)
     else:
-        print("Agenda Vazia.")
+        print("Empty contact list.")
 
 
-def buscaContatos(contatoProcurado):
+def searchContacts(contact):
         try:
-            print(contatoProcurado, ":\n Telefone:", AGENDA[contatoProcurado]['telefone'], "\n Email:", AGENDA[contatoProcurado]["email"], "\n Endereço:", AGENDA[contatoProcurado]["endereco"])
+            print(contact, ":\n Cellphone:", CONTACTLIST[contact]['cellphone'], "\n Email:", CONTACTLIST[contact]['email'], "\n Address:", CONTACTLIST[contact]['address'])
         except KeyError as e:
-            print("Contato {} não encontrado!".format(e))
+            print("Contact {} not found!".format(e))
         except Exception as e:
-            print("Algum erro ocorreu. {}".format(e))
+            print("Error: {}".format(e))
 
 
 
-def inserirContato(contato, telefone, email, endereco):
+def newContact(contact, cellphone, email, address):
     try:
-        AGENDA[contato] = {
-        "telefone":telefone,
+        CONTACTLIST[contact] = {
+        "cellphone":cellphone,
         "email":email,
-        "endereco":endereco,
+        "address":address,
         }
+        print("Contact {} successfully added.".format(contact))
     except Exception as e:
-        print("Erro: {}".format(e))
+        print("Error: {}".format(e))
 
 
-def editarContato(contato):
+def editContact(contact):
     try:
-        # This is only to test if the contact actually exists. / Apenas para checar se o contato existe de fato!
-        AGENDA[contato]
+        CONTACTLIST[contact]
     except KeyError:
-        print("Contato {} não existente.".format(contato))
+        print("contact {} not found".format(contact))
         return
-    print("O que deseja editar?")
-    print("1 - Telefone")
+    print("What do you wnat to edit?: ")
+    print("1 - Cellphone")
     print("2 - Email")
-    print("3 - Endereco")
-    print("4 - Todos")
+    print("3 - Address")
+    print("4 - All")
         
-    acao = input()
-    if(acao == "1"):
-        telefone = input("Digite o novo telefone: ")
-        AGENDA[contato]['telefone'] = telefone
-    elif(acao == "2"):
-        email = input("Digite o novo Email: ")
-        AGENDA[contato]['email'] = telefone
-    elif(acao == "3"):
-        endereco = input("Digite o novo Endereço: ")
-        AGENDA[contato]['endereco'] = endereco
-    elif(acao == "4"):
-        telefone = input("Digite o novo telefone: ")
-        email = input("Digite o novo Email: ")
-        endereco = input("Digite o novo Endereço: ")   
-        AGENDA[contato]['telefone'] = telefone
-        AGENDA[contato]['email'] = email
-        AGENDA[contato]['endereco'] = endereco
+    choice = input()
+    if(choice == "1"):
+        cellphone = input("Input the new Cellphone: ")
+        CONTACTLIST[contact]['cellphone'] = cellphone
+    elif(choice == "2"):
+        email = input("Input the new Email: ")
+        CONTACTLIST[contact]['email'] = email
+    elif(choice == "3"):
+        address = input("Input the new Address: ")
+        CONTACTLIST[contact]['address'] = address
+    elif(choice == "4"):
+        cellphone = input("Input the new Cellphone: ")
+        email = input("Input the new Email: ")
+        address = input("Input the new Address: ")   
+        CONTACTLIST[contact]['cellphone'] = cellphone
+        CONTACTLIST[contact]['email'] = email
+        CONTACTLIST[contact]['address'] = address
     else:
-        print("Opção não existente.")
+        print("Non-existent choice.")
 
 
-def excluirContato(contato):
+def eraseContact(contact):
     try:
-        AGENDA.pop(contato)
+        CONTACTLIST.pop(contact)
+        print("Contact {} successfully erased.".format(contact))
     except KeyError as e:
-        print("Contato {} não existe!".format(e))
+        print("Contact {} doesn't exist.".format(e))
 
         
 def menu():
-    print("O que deseja fazer?: ")
-    print("1 - Adicionar Contato")
-    print("2 - Excluir Contato")
-    print("3 - Alterar dados de Contato")
-    print("4 - Buscar Contato")
-    print("5 - Mostrar lista de Contatos")
-    print("0 - Sair")
+    print("Choose an option: ")
+    print("1 - Add a new contact")
+    print("2 - Erase a contact")
+    print("3 - Edit contact data")
+    print("4 - Search for contact")
+    print("5 - Print contact list")
+    print("6 - Export contacts to CSV file")
+    print("7 - Import contacts from CSV file")
+    print("0 - Exit")
+
+
+def importContacts(fileName):
+    try:
+        with open(fileName, 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                contact = line.strip().split(",")
+                if(contact[0] != "name"):
+                    newContact(contact[0], contact[1], contact[2], contact[3])
+
+    except IOError :
+        print("File not Found.")    
+    except Exception as E:
+        print("Error: {}".format(E))   
+
+
+def exportContacts():
+    try:
+        with open("contact_list.csv", 'w') as file:
+            file.write("name,cellphone,email,address\n")
+            for contact in CONTACTLIST:
+                file.write("{},{},{},{}\n".format(contact, CONTACTLIST[contact]['cellphone'],CONTACTLIST[contact]['email'],CONTACTLIST[contact]['address']))
+        print("Successfully exported contact list!")
+    except Exception as E:
+        print("Error: {}".format(E))
 
 
 while True: 
     menu()
-    acao = input("O que deseja fazer?: ")
-    if(acao == "1"):
-        contatoNovo = input("Digite o nome do contato: ")
-        print("Perfeito!")
-        novoTelefone = input("Digite o telefone do contato: ")
-        novoEmail = input("Digite o email do contato: ")
-        novoEndereco = input("Digite o endereco do contato: ")
-        inserirContato(contatoNovo, novoTelefone, novoEmail, novoEndereco)
-        print("Contato {} inserido!".format(contatoNovo))
-    elif(acao == "2"):
-        contatoExcluir = input("Qual contato deseja excluir?: ") 
-        excluirContato(contatoExcluir)
+    choice = input("Choose an option: ")
+    if(choice == "1"):
+        contact = input("Input the contact name: ")
+        newCellphone = input("Input the contact cellphone: ")
+        newEmail = input("Input the contact email: ")
+        newAddress = input("Input the contact address: ")
+        try:
+            newContact(contact, newCellphone, newEmail, newAddress)
+        except Exception as E:
+            print(E)
+    elif(choice == "2"):
+        erasedContact = input("Which contact do you want to erase?: ") 
+        eraseContact(erasedContact)
         
-    elif(acao == "3"):
-        contatoEditar = input("Qual contato deseja editar?: ")
-        editarContato(contatoEditar)
-    elif(acao == "4"):
-        contatoBusca = input("Qual o nome do seu contato?: ")
-        buscaContatos(contatoBusca)
-    elif(acao == "5"):
-        mostraContatos()
-    elif(acao == "0"):
+    elif(choice == "3"):
+        editedContact = input("Which contact do you want to edit?: ")
+        editContact(editedContact)
+    elif(choice == "4"):
+        searchedContact = input("Please input the contact name: ")
+        searchContacts(searchedContact)
+    elif(choice == "5"):
+        showContacts()
+    elif(choice == "6"):
+        exportContacts()
+    elif(choice == "7"):
+        fileName = input("Input the file name: ")
+        importContacts(fileName)
+    elif(choice == "0"):
         break
     else:
-        print("Opção inválida")
+        print("Invalid Option.")
